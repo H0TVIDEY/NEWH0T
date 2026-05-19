7a7aefabb93fabbe5b5c7c10be2b9a1ecdc57d54
H0TVIDEY <Keisyaalitia@gmail.com>
2026-05-19 16:54:47 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+console.log("Hello, World!");
+$ npm init
+  ...
+  package name: @H0TVIDEY/NEWH0T
+  ...
+  test command: exit 0
+  ...
+  npm install
+git add index.js package.json package-lock.json
+git commit -m "initialize npm package"
+git push
